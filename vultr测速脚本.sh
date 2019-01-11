13cff439bed1ec188b7abf883f82d3133d56995d
whoami?? <www1075534170@qq.com>
2019-01-12 00:16:09 +0800

    Add files via upload

---
+++ vultr测速脚本.sh
@@ -0,0 +1,32 @@
+echo "01 Tokyo"
+ping hnd-jp-ping.vultr.com -f  -c 1000
+echo "02 Singapore"
+ping sgp-ping.vultr.com -f  -c 1000
+echo "03 Amsterdam"
+ping ams-nl-ping.vultr.com -f  -c 1000
+echo "04 Paris"
+ping par-fr-ping.vultr.com -f  -c 1000
+echo "05 Frankfurt"
+ping fra-de-ping.vultr.com -f  -c 1000
+echo "06 London"
+ping lon-gb-ping.vultr.com -f  -c 1000
+echo "07 New York"
+ping nj-us-ping.vultr.com -f  -c 1000
+echo "08 Chicago"
+ping il-us-ping.vultr.com -f  -c 1000
+echo "09 Dallas"
+ping tx-us-ping.vultr.com -f  -c 1000
+echo "10 Atlanta"
+ping ga-us-ping.vultr.com -f  -c 1000
+echo "11 Los Angeles"
+ping lax-ca-us-ping.vultr.com -f  -c 1000
+echo "12 Miami"
+ping fl-us-ping.vultr.com -f  -c 1000
+echo "13 Seattle"
+ping wa-us-ping.vultr.com -f  -c 1000
+echo "14 Silicon Valley"
+ping sjo-ca-us-ping.vultr.com -f  -c 1000
+echo "15 Sydney"
+ping syd-au-ping.vultr.com -f  -c 1000
+echo "16 tor"
+ping tor-ca-ping.vultr.com -f  -c 1000
